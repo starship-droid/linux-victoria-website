266e531a44951ff2502a9ff91780d5adf53c3028
starship-droid <github@sophieallen.space>
2025-05-06 16:04:20 +1000

    redirects plugin removed from eleventy.js to fix build fail

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,3 @@
-const redirectsPlugin = require('eleventy-plugin-redirects');
-
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.addPlugin(redirectsPlugin, {
-        template: 'netlify', // netlify, vercel or clientSide
-    })
 };
